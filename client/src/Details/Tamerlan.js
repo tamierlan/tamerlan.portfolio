acf9d51419561516e33789c9f88aad7a44bfec3f
tamerlan ermanbet uulu <tommy-eduardo@tamerlans-MBP.home>
2020-04-05 18:32:54 -0400

    sdfs

--- client/src/Details/Tamerlan.js
+++ client/src/Details/Tamerlan.js
@@ -3,9 +3,12 @@ import React from 'react';
 
 export default Tamerlan => {
   return (
-    <div>
-      <h1 className="title">Tamerlan <br/> Ermanbet</h1>
+    <>
+      <div className='tommy-eduard'>
+        <h1 className='title'>Tamerlan </h1>
+        <h1 className='title'>Ermanbet</h1>
+      </div>
       <h3>web developer</h3>
-    </div>
+    </>
   )
 }
