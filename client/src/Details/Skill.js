f2a4ea2cc156051ae1ac36300993f9d5563367bf
tamerlan ermanbet uulu <tommy-eduardo@tamerlans-MBP.home>
2020-04-06 12:33:50 -0400

    dfga

--- client/src/Details/Skill.js
+++ client/src/Details/Skill.js
@@ -8,25 +8,7 @@ import { FaNodeJs } from 'react-icons/fa';
 export default Skill => {
   return (
     <div className='skill'>
-      <div className='icon-text'>
-        <TiHtml5 className='icon' /><h2>html</h2>
-      </div>
-      <div className='icon-text'>
-        <FaCss3Alt className='icon' />
-        <h2>css / bootstrap</h2>
-      </div>
-      <div className='icon-text'>
-        <IoLogoJavascript className='icon' />
-        <h2>javascript</h2>
-      </div>
-      <div className='icon-text'>
-        <FaReact className='icon' />
-        <h2>react / context api</h2>
-      </div>
-      <div className='icon-text'>
-        <FaNodeJs className='icon' />
-        <h2>node js</h2>
-      </div>
+      Skill
     </div>
   )
 }
