b9a0cdf988e359eb1ff27fed6a9f0dc1d31b80df
tamerlan ermanbet uulu <tommy-eduardo@tamerlans-MBP.home>
2020-04-07 18:32:01 -0400

    phone compureter added

--- client/src/Projects/Projects.js
+++ client/src/Projects/Projects.js
@@ -1,81 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import imac from '../images/mockup.png'
 
 export default Projects => {
+  const [state] = useState([
+    {
+      title: "Cellphone Store",
+      subtitle: "( fullstack react website )",
+      link: "https://phone-works.herokuapp.com/",
+      text: "If you’re a student or a professional looking to get ahead, you need a website. It’s your chance to establish an online home base, impress recruiters, and provide something that will set you apart from the stacks of static resumes that everyone else is submitting for job applications."
+    },
+    {
+      title: "Cellphone Store",
+      subtitle: "( fullstack react website )",
+      link: "https://phone-works.herokuapp.com/",
+      text: "If you’re a student or a professional looking to get ahead, you need a website. It’s your chance to establish an online home base, impress recruiters, and provide something that will set you apart from the stacks of static resumes that everyone else is submitting for job applications."
+    },
+    {
+      title: "Cellphone Store",
+      subtitle: "( fullstack react website )",
+      link: "https://phone-works.herokuapp.com/",
+      text: "If you’re a student or a professional looking to get ahead, you need a website. It’s your chance to establish an online home base, impress recruiters, and provide something that will set you apart from the stacks of static resumes that everyone else is submitting for job applications."
+    }
+
+  ])
+
   return(
     <div className="projects">
       <div className='hoveronweb'>
-        <div className="project">
-        <a href='https://phone-works.herokuapp.com/'>
-          <img src={imac} alt="imac" />
-          <h3 className="title">Cellphone Store</h3>
-          <h4>( fullstack real website )</h4>
-          <p>
-            If you’re a student or a professional
-            looking to get ahead, you need a website.
-            It’s your chance to establish an online home base,
-            impress recruiters,
-            and provide something that will set you apart from the
-            stacks of static resumes that everyone else is
-            submitting for job applications.
-          </p>
-          <button>
-              Check Out
-          </button>
-        </a>  
-        </div>
-      </div>
-
-      <div className='hoveronweb'>
-        <div className="project">
-          <img src={imac} alt="imac" />
-          <h3 className="title">Cellphone Store</h3>
-          <h4>( fullstack real website )</h4>
-          <p>
-            If you’re a student or a professional
-            looking to get ahead, you need a website.
-            It’s your chance to establish an online home base,
-            impress recruiters,
-            and provide something that will set you apart from the
-            stacks of static resumes that everyone else is
-            submitting for job applications.
-          </p>
-          <button>Check Out</button>
-        </div>
-      </div>
-      <div className='hoveronweb'>
-        <div className="project">
-          <img src={imac} alt="imac" />
-          <h3 className="title">Cellphone Store</h3>
-          <h4>( fullstack real website )</h4>
-          <p>
-            If you’re a student or a professional
-            looking to get ahead, you need a website.
-            It’s your chance to establish an online home base,
-            impress recruiters,
-            and provide something that will set you apart from the
-            stacks of static resumes that everyone else is
-            submitting for job applications.
-          </p>
-          <button>Check Out</button>
-        </div>
-      </div>
-      <div className='hoveronweb'>
-        <div className="project">
-          <img src={imac} alt="imac" />
-          <h3 className="title">Cellphone Store</h3>
-          <h4>( fullstack real website )</h4>
-          <p>
-            If you’re a student or a professional
-            looking to get ahead, you need a website.
-            It’s your chance to establish an online home base,
-            impress recruiters,
-            and provide something that will set you apart from the
-            stacks of static resumes that everyone else is
-            submitting for job applications.
-          </p>
-          <button>Check Out</button>
-        </div>
+        {state.map(item => (
+          <div className="project">
+            <a className="computer" href={item.link}>
+              <img src={imac} alt="imac" />
+              <h3 className="title">{item.title}</h3>
+              <h4>{item.subtitle}</h4>
+              <p>{item.text}</p>
+              <button>
+                Check Out
+              </button>
+            </a>
+            <a className="phone">
+              <img src={imac} alt="imac" />
+              <h3 className="title">{item.title}</h3>
+              <h4>{item.subtitle}</h4>
+              <p>{item.text}</p>
+              <a href={item.link}>
+                <button>
+                  Check Out
+                </button>
+              </a>  
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
